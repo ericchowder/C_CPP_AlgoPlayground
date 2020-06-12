edbd0c77c0646e3a4b43f81810d8a873ea935fe2
linksta7 <linksta7@gmail.com>
2020-06-12 18:26:29 -0400

    MSVC solution created, added gitignore into sub directory

---
+++ extra/extra.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    printf("Hello World!\n");
+    printf("take 4\n");
+
+    vector<int> g1;
+
+    return 0;
+}
---
+++ extra/vectors.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void myVector();
+
+int main()
+{
+    printf("Hellow world!");
+    myVector();
+    return 0;
+}
+
+// Vectors are used as arrays, but are dynamic thus can be resized
+void myVector()
+{
+    // Instantiate Vector
+    vector<int> vectorVar;
+
+}
