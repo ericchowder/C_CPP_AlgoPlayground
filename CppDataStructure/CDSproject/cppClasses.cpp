0849d9a36249e4a1038171be37f9d8a83a899500
linksta7 <linksta7@gmail.com>
2020-06-18 17:55:43 -0400

    added private method to conv 1|0 to true|false

--- CppDataStructure/CDSproject/cppClasses.cpp
+++ CppDataStructure/CDSproject/cppClasses.cpp
@@ -18,10 +18,28 @@ public:
 		height = h;
 	}
 
+	// member function (method) for displaying animal properties
+	void displayInfo() {
+		cout << "My animal is a " << name << ".\n"
+			<< "It is quadrupedal: "; numToBool(quadrupedalism); cout << ".\n"
+			<< "They are normally " << height << "m tall/long, "
+			<< "and " << weight << "kg heavy.\n";
+	}
+
 // private members
 private:			
 	bool endangered;
 	string countryOrigin;
+
+	// convert bool output of 1|0 to true|false
+	void numToBool(bool boolVal) {
+		if (boolVal = true)
+			cout << "true";
+		else if (boolVal = false)
+			cout << "false";
+		else
+			cout << "invalid bool value";
+	}
 };
 
 void cppClasses() {
@@ -33,4 +51,7 @@ void cppClasses() {
 	// Instantiating animal with overloaded constructor
 	myAnimal myPlatypus(true, 1.5, 0.38);
 	myPlatypus.name = "platypus";
+
+	cout << "Using member function 'displayInfo()' to print properties:\n";
+	myPlatypus.displayInfo();
 }
